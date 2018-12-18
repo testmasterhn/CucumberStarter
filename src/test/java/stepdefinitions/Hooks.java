fe536cade9b7b85c8101209ce58cfee58c40360c
testmasterhn <45918228+testmasterhn@users.noreply.github.com>
2018-12-18 11:10:39 +0700

    Update hook support multiple browser

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -5,29 +5,40 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
 public class Hooks {
-	
+
 	public static WebDriver driver;
-	
+
 	@Before
 	public void BeforeScenario() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
-		driver = new ChromeDriver();
+		String webdriver = System.getProperty("browser", "firefox");
+		switch (webdriver) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", "C:\\\\Selenium\\\\Driver\\\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		default:
+            throw new RuntimeException("Unsupported webdriver: " + webdriver);
+		}
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 	}
-	
+
 	@After
 	public void AfterScenario(Scenario scenario) {
-		if(scenario.isFailed())
-		{
+		if (scenario.isFailed()) {
 			try {
-				byte[]screenshot =((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+				byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 				scenario.embed(screenshot, "image/png");
 			} catch (WebDriverException platformdontsupportscreenshot) {
 				System.err.println(platformdontsupportscreenshot.getMessage());
